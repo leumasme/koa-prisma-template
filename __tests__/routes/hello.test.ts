210e6f368d963477eb0c5ad97d1d369cb56f938d
Temm <samumert@gmail.com>
2021-02-09 18:55:00 +0100

    Add Test for Hello Route

---
+++ __tests__/routes/hello.test.ts
@@ -0,0 +1,18 @@
+import server from "../../src/server";
+import request from "supertest";
+
+// close the server after each test
+afterEach((done) => {
+  server.close();
+  done();
+});
+
+describe("routes/hello", () => {
+  it("should hello", async () => {
+    const response = await request(server).get("/hello");
+    expect(response.status).toEqual(200);
+    expect(response.type).toEqual("application/json");
+    expect(response.body.status).toEqual("success");
+    expect(response.body.data).toEqual("hello");
+  });
+});
