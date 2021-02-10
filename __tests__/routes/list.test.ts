2948660911d3c51630e2011fb299f24d9cb5e0b8
Temm <samumert@gmail.com>
2021-02-10 18:23:52 +0100

    Implement List Route

--- __tests__/routes/list.test.ts
+++ __tests__/routes/list.test.ts
@@ -3,13 +3,20 @@ import request from "supertest";
 
 describe("routes/list", () => {
   it("should add an item to the list", async () => {
-    const response = await request(server.callback()).post("/list").send({ "data": "My Element" });
+    const response = await request(server.callback())
+      .post("/list")
+      .send({ data: "My Element" });
     expect(response.status).toEqual(201);
+    expect(response.type).toEqual("application/json");
+    expect(response.body).toEqual({ success: true });
   });
   it("should list all added items", async () => {
     const response = await request(server.callback()).get("/list");
     expect(response.status).toEqual(200);
     expect(response.type).toEqual("application/json");
-    expect(response.body).toEqual(["My Element"]);
+    expect(response.body).toEqual({
+      success: true,
+      data: ["My Element"],
+    });
   });
 });
---
+++ src/routes/list.ts
@@ -0,0 +1,31 @@
+import Router from "koa-router";
+const router = new Router();
+
+var elems: string[] = [];
+
+router
+  .post(`/list`, async (ctx) => {
+    if (
+      ctx.request.body == null ||
+      ctx.request.body == undefined ||
+      typeof ctx.request.body.data != "string"
+    ) {
+      ctx.throw("Invalid Body", 400);
+      return;
+    }
+
+    elems.push(ctx.request.body.data);
+
+    ctx.body = {
+      success: true,
+    };
+    ctx.status = 201;
+  })
+  .get(`/list`, async (ctx) => {
+    ctx.body = {
+      success: true,
+      data: elems,
+    };
+  });
+
+export default router;
